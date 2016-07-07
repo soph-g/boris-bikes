2b567413bac878b4747b950b3ed45b2a14d7865a
Georgia Mills <georgee_123@hotmail.com>
2016-07-07 16:29:29 +0100

    made methods private

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,15 +8,17 @@ class DockingStation
 
 
   def release_bike
-      fail "No bikes available" if self.empty?
+      fail "No bikes available" if empty?
       @bikes.pop
     end
 
   def dock(bike)
-    fail "Docking station full" if self.full?
+    fail "Docking station full" if full?
     @bikes << bike
   end
 
+
+private
   def full?
     @bikes.count >= 20 ? true : false
   end
@@ -24,6 +26,5 @@ class DockingStation
   def empty?
     @bikes.empty?
   end
-
 
 end
