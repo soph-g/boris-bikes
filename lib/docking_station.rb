78547d5ce8bad643cfd75798028dc3276e971caa
Georgia Mills <georgee_123@hotmail.com>
2016-07-07 14:13:18 +0100

    Initilialized empty array and changed unit tests and code so limit is 20 bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,15 +2,19 @@ require_relative 'bike'
 
 class DockingStation
   attr_reader :bikes
-
-  def dock(bike)
-    fail "Docking station full" if @bikes
-    @bikes = bike
+  def initialize
+    @bikes = []
   end
 
+
   def release_bike
-    fail "No bikes available" unless @bikes
-    @bikes
+      fail "No bikes available" if @bikes.empty?
+      @bikes.pop
+    end
+
+  def dock(bike)
+    fail "Docking station full" if @bikes.count >= 20
+    @bikes << bike
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,11 +5,12 @@ describe DockingStation do
   describe '#dock' do
     it 'docks a bike' do
       bike = Bike.new
-      expect(subject.dock(bike)).to eq bike
+      expect(subject.dock(bike)).to eq [bike]
     end
-    it 'raises an error when more than one bike is docked' do
-      subject.dock(Bike.new)
-      expect{subject.dock(Bike.new)}.to raise_error "Docking station full"
+    it 'raises an error when full' do
+      20.times {subject.dock(Bike.new)}
+      expect {subject.dock(Bike.new)}.to raise_error "Docking station full"
+
     end
   end
 
@@ -28,7 +29,7 @@ describe DockingStation do
     it 'returns docked bikes' do
       bike = Bike.new
       subject.dock(bike)
-      expect(subject.bikes).to eq bike
+      expect(subject.bikes).to eq [bike]
     end
   end
 end
