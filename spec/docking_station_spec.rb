e89832d44f102aa98cfd22abc15c0bb83bc3513b
Sophie Gill <sophie-gill@hotmail.com>
2016-07-07 10:43:32 +0100

    changed release_bike to release only available bike, and added fail error. Included Rspec tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,25 +1,37 @@
 require "docking_station"
 
 describe DockingStation do
-  it {is_expected.to respond_to(:release_bike)}
 
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  describe '#release_bike' do
+    it 'raises an error when no bikes are available' do
+      expect{subject.release_bike}.to raise_error "No bikes available"
+    end
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
   end
 
-  it {is_expected.to respond_to(:dock).with(1).argument}
-  it {is_expected.to respond_to(:bike)}
-
-  it 'docks bikes' do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+  describe '#dock' do
+    it 'docks a bike' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+    end
   end
 
-  it 'returnes docked bikes' do
+  it 'returns docked bikes' do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
   end
-
 end
+
+
+
+
+
+
+
+
+#end
