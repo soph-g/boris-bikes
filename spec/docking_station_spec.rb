bff5c85f1df5e9bb8e57083b596def6ef7532d44
Sophie Gill <sophie-gill@hotmail.com>
2016-07-08 13:41:13 +0100

    continued refactoring docking_station_spec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -13,34 +13,38 @@ describe DockingStation do
   end
 
   describe '#dock' do
-    let(:bike) { double :bike }
+    #let(:bike) { double :bike }
     it 'docks a working bike' do
+      bike = double(:bike)
       expect(subject.dock(bike)).to eq [bike]
     end
     it 'raises an error when full' do
+      bike = double(bike)
       subject.capacity.times {subject.dock(bike)}
       expect {subject.dock(bike)}.to raise_error "Docking station full"
     end
     it 'docks a broken bike' do
-      allow(bike).to receive(:broken?).and_return(true)
+      bike = double(:bike, broken?: true)
+      #allow(bike).to receive(:broken?).and_return(true)
       expect(subject.dock(bike)).to eq [bike]
     end
   end
 
   describe '#release_bike' do
-    let(:bike) { double :bike }
-
+    #let(:bike) { double :bike }
     it 'raises an error when no bikes are available' do
       expect{subject.release_bike}.to raise_error "No bikes available"
     end
     it 'releases a working bike' do
-      allow(bike).to receive(:broken?).and_return(false)
+      bike = double(:bike, broken?: false)
+      #allow(bike).to receive(:broken?).and_return(false)
       subject.dock(bike)
-      released_bike = subject.release_bike
-      expect(released_bike).not_to be_broken
+      #released_bike = subject.release_bike
+      expect(subject.release_bike).not_to be_broken
     end
     it 'does not release broken bikes' do
-      allow(bike).to receive(:broken?).and_return(true)
+      bike = double(:bike, broken?: true)
+      #allow(bike).to receive(:broken?).and_return(true)
       subject.dock(bike)
       expect{subject.release_bike}.to raise_error "Bike is broken, cannot be released"
     end
